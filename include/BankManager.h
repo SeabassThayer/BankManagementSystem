efe5f5bcd9aad0279dd9d0f42c26efac9c613261
Damian Olivo <olivodamian2@gmail.com>
2025-12-08 01:19:07 -0600

    Bare Minimum Accomplished
    the program compiles and executes now. Of course, nothing happens yet because nothing is implemented yet.

--- include/BankManager.h
+++ include/BankManager.h
@@ -6,16 +6,16 @@
 #include "User.h"
 
 class BankManager {
+private:
+    std::string username;
+    std::string password;
+
 public:
     BankManager(const std::string& username, const std::string& password);
     
     bool login(const std::string& username, const std::string& password);
     void viewAllUsers(const std::vector<User>& users);
     void manageUserAccounts();
-    
-private:
-    std::string username;
-    std::string password;
 };
 
 #endif // BANKMANAGER_H
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "BankAccount.h"
+#include "BankManager.h"
+#include "LoginManager.h"
+#include "Transaction.h"
+#include "User.h"
+
+#include <string>
+#include <iostream>
+
+int main() 
+{
+	return 0;
+}
