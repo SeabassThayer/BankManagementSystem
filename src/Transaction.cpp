1b6b83ae9b8ce03f36ae3b37b1f252d1b1c50a7d
CaptainLemur840 <115422719+CaptainLemur840@users.noreply.github.com>
2025-12-09 18:42:53 -0600

    Added basic functionality

--- src/Transaction.cpp
+++ src/Transaction.cpp
@@ -1,1 +1,26 @@
 #include "Transaction.h"
+#include <string>
+
+Transaction::Transaction(const std::string& accountNumber, TransactionType type, double amount) : accountNumber(accountNumber), type(type), amount(amount)
+{
+	
+
+}
+
+std::string Transaction::getAccountNumber() const
+{
+	return accountNumber;
+}
+Transaction::TransactionType Transaction::getType() const
+{
+	return type;
+}
+double Transaction::getAmount() const
+{
+	return amount;
+}
+std::string Transaction :: getTimestamp() const
+{
+	return timestamp;
+}
+
