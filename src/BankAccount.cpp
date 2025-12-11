ded6d92b22a9cd1230bd293355ebb4a37a8e0e6d
Damian Olivo <olivodamian2@gmail.com>
2025-12-10 23:41:53 -0600

    Added comments (Part 3)
    added comments to the implementation file for BankManager

--- src/BankAccount.cpp
+++ src/BankAccount.cpp
@@ -7,11 +7,14 @@
 
 int BankAccount::totalBankAccounts = 0;
 
+// if accID is -1,we assume Bank Account is being created at run-time
+// if accID is not -1, we assume Bank Account is being retrieved from file system. 
 BankAccount::BankAccount(int cusID, BankAccountType type, double initBalance, int accID)
     :customerID(cusID), accountType(type), balance(initBalance)
 {
+    // if accID = -1, a new unique ID will be created and assigned. 
+    // and account will be marked as new during constructor execution. 
     bool newAccount = false;
-
     if (accID == -1) {
         std::cout << "Creating new bank account. IDs will be assigned." << std::endl;
         newAccount = true;
@@ -21,11 +24,15 @@ BankAccount::BankAccount(int cusID, BankAccountType type, double initBalance, in
         bankAccountID = accID;
     }
 
+    // catch invalid Bank Account ID if it slipped previous check. 
     if (bankAccountID == -1) {
         throw std::runtime_error("bankAccountID was not able to be assigned");
     }
 
+    // execute if Bank Account is marked as new.
     if (newAccount) {
+        // use relative path and unique ID to make a file where the data for this 
+        // Bank Account will be stored to use between sessions. 
         std::ofstream accountFile("../../../data/bankAccounts/" + std::to_string(bankAccountID) + ".txt");
         if (accountFile.is_open()) {
             accountFile << bankAccountID << std::endl;
@@ -44,10 +51,12 @@ BankAccount::BankAccount(int cusID, BankAccountType type, double initBalance, in
     std::cout << "Bank account (" << bankAccountID << ") has been loaded/created." << std::endl;
 }
 
+// retrieve new unique Bank Account ID using relative paths
 int BankAccount::getNewID() const {
     std::ifstream inputFile("../../../data/IDtrackers/bankAccountID.txt");
     int id = -1;
 
+    // retrieve unique ID from file. 
     if (inputFile.is_open()) {
         inputFile >> id;
         inputFile.close();
@@ -56,6 +65,7 @@ int BankAccount::getNewID() const {
         std::cout << "ERROR: unable to open \"bankAccountID.txt\" for READING" << std::endl;
     }
 
+    // if unique ID was retrieved from file, new unique ID is inserted into the file
     if (id != -1) {
         std::ofstream outputFile("../../../data/IDtrackers/bankAccountID.txt");
         if (outputFile.is_open()) {
@@ -75,6 +85,7 @@ BankAccount::~BankAccount()
     std::cout << "WARNING: Bank account is being removed from program memory" << std::endl;
 }
 
+// return all transactions saved by Class instance on it's transactions variable. 
 const std::vector<Transaction>& BankAccount::getTransactions() const {
     return transactions;
 }
@@ -95,6 +106,7 @@ void BankAccount::deposit(double amount)
 }
 
 void BankAccount::withdraw(double amount) {
+    // perform basic validity checks before allowing Customer to withdraw money. 
     if (amount <= 0) {
         std::cout << "ERROR: Withdraw amount must be positive." << std::endl;
         return;
@@ -106,11 +118,12 @@ void BankAccount::withdraw(double amount) {
     {
         balance -= amount;
         std::cout << "Withdrawn: $" << amount << std::endl << "New Balance: $" << balance << std::endl;
-        transactions.emplace_back(customerID, Transaction::AccountTransactionType::WITHDRAWAL, amount, 0, balance, "time",-1);
+        transactions.emplace_back(customerID, Transaction::AccountTransactionType::WITHDRAWAL, amount, 0, balance, "time unspecified",-1);
     }
 
 }
 
+// print account summary plus all transactions, if any. 
 void BankAccount::printAccountSummary() const {
     std::cout << "===============================" << std::endl;
     std::cout << "========Account Summary========" << std::endl;
@@ -120,6 +133,7 @@ void BankAccount::printAccountSummary() const {
     std::cout << "Current Balance: $" << balance << std::endl;
     std::cout << "------ Transaction History ------" << std::endl;
 
+    // checking if bank account has any transactions
     if (transactions.empty()) {
         std::cout << "No transactions found." << std::endl;
     }
@@ -130,6 +144,7 @@ void BankAccount::printAccountSummary() const {
     }
 }
 
+// use relative path to get total bank account data from file.
 void BankAccount::loadTotalBankAccounts()
 {
     std::ifstream inputFile("../../../data/bankInfo/totalBankAccounts.txt");
@@ -142,6 +157,7 @@ void BankAccount::loadTotalBankAccounts()
     }
 }
 
+// use relative path to save total bank account data to file. 
 void BankAccount::saveTotalBankAccounts() {
     std::ofstream outputFile("../../../data/bankInfo/totalBankAccounts.txt");
     if (outputFile.is_open()) {
@@ -152,6 +168,8 @@ void BankAccount::saveTotalBankAccounts() {
         throw std::runtime_error("Unable to save totalBankAccounts.txt");
     }
 }
+
+                            // getters //
 
 int BankAccount::getTotalBankAccounts() { return totalBankAccounts; }
 
