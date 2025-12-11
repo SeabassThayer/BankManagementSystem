d3b1115a7b449d2fed918ffac31bd2d392496947
Damian Olivo <olivodamian2@gmail.com>
2025-12-11 01:23:02 -0600

    Bare bones implementation
    started the implementation of the LoginManager

--- src/LoginManager.cpp
+++ src/LoginManager.cpp
@@ -1,1 +1,57 @@
 #include "LoginManager.h"
+#include <iostream>
+
+LoginManager::LoginManager() {}
+
+LoginManager::~LoginManager() {}
+
+void LoginManager::run() 
+{
+    bool running = true;
+    while (running) {
+        showMainMenu();
+        int choice;
+        std::cin >> choice;
+
+        // handles user input that is not a number between 1 and 4
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input. Please enter a number between 1 and 4.\n";
+            continue;
+        }
+
+        switch (choice) {
+            case 1: handleUserLogin(); break;
+            case 2: handleAccountCreation(); break;
+            case 3: handleManagerLogin(); break;
+            case 4: running = false; break;
+            default: std::cout << "Invalid option. Try again.\n"; break;
+        }
+    }
+}
+
+void LoginManager::showMainMenu() 
+{
+    std::cout << "\nWelcome to Awesome Bank\n";
+    std::cout << "1. User Login\n";
+    std::cout << "2. Create Account\n";
+    std::cout << "3. Manager Login\n";
+    std::cout << "4. Exit\n";
+    std::cout << "Choose an option: ";
+}
+
+void LoginManager::handleUserLogin() 
+{
+
+}
+
+void LoginManager::handleAccountCreation() 
+{
+
+}
+
+void LoginManager::handleManagerLogin()
+{
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,8 @@
-#include "BankAccount.h"
-#include "BankManager.h"
 #include "LoginManager.h"
-#include "Transaction.h"
-#include "User.h"
-#include "Customer.h"
-
-#include <string>
-#include <iostream>
-#include <stdexcept>
 
 int main() 
 {
-	try {
-		Customer::loadTotalCustomers();
-		BankAccount::loadTotalBankAccounts();
-		Customer me("SunnyLord", "SUNNY20lord", "sunny", "lord", 114, 2);
-		std::cout << "user id: " << me.getUserID() << std::endl;
-		std::cout << "customer id: " << me.getCustomerID() << std::endl;
-		std::cout << "username: " << me.getUsername() << std::endl;
-		std::cout << "password: " << me.getPassword() << std::endl;
-		std::cout << "first name: " << me.getFirstName() << std::endl;
-		std::cout << "last name: " << me.getLastName() << std::endl;
-		std::cout << "total customers: " << me.getTotalCustomers() << std::endl;
-
-		BankAccount sav1(2, BankAccount::BankAccountType::CHECKINGS, 256, 2);
-
-		Transaction tran1(0, Transaction::AccountTransactionType::DEPOSIT, 50, 5, 55, "big ol time", -1);
-	}
-	catch (const std::runtime_error& e) {
-		std::cerr << "ERROR: could not create user: " << e.what() << std::endl;
-	}
-
+	LoginManager manager;
+	manager.run();
 	return 0;
 }
